c04f0bffd5d0b62e77b37e38f99e2bcd119b9358
MirkoEberlein <mail@mirko-eberlein.de>
2016-10-23 16:46:07 +0200

    letzter Stand Probleme Angular

--- currency-webui/src/main/resources/ui/Home/currencyEndpoint.factory.js
+++ currency-webui/src/main/resources/ui/Home/currencyEndpoint.factory.js
@@ -1,16 +1,19 @@
 (function() {
 	"use strict";
 
-	angular.module('currencyWeb').factory(
-			'currencyEndpoint',
-			[ '$resource' ,function($resource) {
+	angular.module('currencyWeb').factory('currencyEndpoint',[ '$resource' , function($resource) {
 
-				var options = { 'query':  { isArray:false} };
-				
-				var currencyEndpoint = $resource('${rest-base-url}/getall',
-						{}, options);
-				
-				
-				return currencyEndpoint;
-			}]);
+		var currencyEndpoint = {};
+		var options = {
+			'query' : {
+				isArray : false
+			}
+		};
+
+	
+
+		return currencyEndpoint;
+	}]);
+
 })();
+
