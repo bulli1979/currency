842d7eb5e0a48b53ccb5b2599d1aa178bdc973bb
Mirko Eberlein <mirko.eberlein@webgate.biz>
2016-11-10 07:43:52 +0100

    no message

--- curency-ee-db/src/main/webapp/content/ui/Home/currencyEndpoint.factory.js
+++ curency-ee-db/src/main/webapp/content/ui/Home/currencyEndpoint.factory.js
@@ -1,42 +1,43 @@
 (function() {
 	"use strict";
 
-	angular.module('currencyWeb').factory(
-			'CurrencyEndpoint',
-			[
-					'$resource',
-					function($resource) {
-						var CurrencyEndpoint = {};
+	angular
+			.module('currencyWeb')
+			.factory(
+					'CurrencyEndpoint',
+					[
+							'$resource',
+							function($resource) {
+								var CurrencyEndpoint = {};
 
-						var options = {
-							'query' : {
-								isArray : false
-							}
-						};
+								var options = {
+									'query' : {
+										isArray : false
+									}
+								};
 
-						var allPath = $resource('/currency/service/api/getall', {},
-								{});
+								var allPath = $resource(
+										'http://localhost:8080/currency-ee-db/service/api/getall', {}, {});
+								var calculatePath = $resource(
+										'http://localhost:8080/currency-ee-db/service/api/change/:amount/:from/:to/',
+										{
+											amount : "@amount",
+											from : "@from",
+											to : "@to"
+										}, options);
 
-						var calculatePath = $resource(
-								'/currency/service/api/change/:amount/:from/:to/', {
-									amount : "@amount",
-									from : "@from",
-									to : "@to"
-								}, options);
+								CurrencyEndpoint.getAll = function() {
+									return allPath.query({});
+								};
 
-						CurrencyEndpoint.getAll = function() {
-							return allPath.query({});
-						};
+								CurrencyEndpoint.calculate = function(amount, from, to, succ, err) {
+									return calculatePath.get({
+										"amount" : amount,
+										"from" : from,
+										"to" : to
+									},succ,err);
+								}
 
-						CurrencyEndpoint.calculate = function(amount, from, to,
-								succ, err) {
-							return calculatePath.get({
-								"amount" : amount,
-								"from" : from,
-								"to" : to
-							}, succ, err);
-						}
-
-						return CurrencyEndpoint;
-					} ]);
+								return CurrencyEndpoint;
+							} ]);
 })();
