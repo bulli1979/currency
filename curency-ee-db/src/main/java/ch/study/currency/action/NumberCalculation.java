e7c7625542d8dd18c149fb5b8cc8f47b108efdfe
gitificial <federer.michi@gmail.com>
2016-12-11 22:34:52 +0100

    Update NumberCalculation.java

--- curency-ee-db/src/main/java/ch/study/currency/action/NumberCalculation.java
+++ curency-ee-db/src/main/java/ch/study/currency/action/NumberCalculation.java
@@ -1,23 +1,41 @@
 package ch.study.currency.action;
 
+
+/**
+ * @author Mirko Eberlein
+ * @version 0.1
+ *
+ */
+
+/**
+ * The NumberCalculation class provides the necessary basic math operation to
+ * convert an initial value with specific currency to a target value with a specific currency. 
+ * Furthermore it provides an operation to calculate the ratio between two currencies.
+ * 
+ */
 public class NumberCalculation {
 	
-	/**Klasse stellt eine Ergebnis für einen eingabe Wert her
-	 * Ich gebe von zu in relation zu einer Basis und bekomme ein Ergebnis. 
-	 * @param from Ausgangswert
-	 * @param to Zielwert
-	 * @param value Anzahl Einheiten
-	 * @return Wieviel von Zielwert ergibt Ausgangswert
+	/**
+	 * Calculates the currency amount of the input value.
+	 * 
+	 * @param from 		base currency rate
+	 * @param to 		target currency rate
+	 * @param value 	initial currency value
+	 * 
+	 * @return 	target currency value
 	 */
 	public static double change(double from,double to,double value){
 		return 1/from * to * value;
 	}
 	
 	
-	/** Gibt eine relation 2er Werte zurück
-	 * @param from Ausgangswert
-	 * @param to Zielwert
-	 * @return
+	/** 
+	 * Returns the ratio between "from" and "to" value
+	 * 
+	 * @param from 	base currency rate
+	 * @param to 	target currency rate
+	 * 
+	 * @return ratio between initial and target currency rate
 	 */
 	public static double getRelation(double from,double to){
 		return 1/from * to;
