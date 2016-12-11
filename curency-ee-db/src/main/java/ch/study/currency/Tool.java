4c049dfe0671a56b49c0d8d8dbfe3dc3b80ab22b
gitificial <federer.michi@gmail.com>
2016-12-11 22:21:01 +0100

    Update Tool.java

--- curency-ee-db/src/main/java/ch/study/currency/Tool.java
+++ curency-ee-db/src/main/java/ch/study/currency/Tool.java
@@ -11,8 +11,9 @@ import ch.study.currency.data.DAOCurrency;
 
 /**
  * @author Mirko Eberlein
- * Enum with static helper functions
- * Using Enum as Singelton see Effective Java 
+ *
+ * The Tool class provides basic operations to convert strings into Date objects and reverse.
+ * An Enumerator with static helper functions. (Using Enum as Singelton see Effective Java.)
  *
  */
 public enum Tool {
@@ -21,27 +22,31 @@ public enum Tool {
 	private SimpleDateFormat formater = new SimpleDateFormat("yyyy-MM-dd");
 
 	/**
-	 * @param dateString need format yyyy-MM-dd (sqllite)
-	 * @return a Date created from the given String
-	 * @throws ParseException
+	 * Converts a string which contains a date to a Date object.
+	 * @param dateString string to be converted to a Date object. Target format yyyy-MM-dd (required by the SQLite-database)
+	 * @return a Date object created from the given String
+	 * @throws ParseException	Signals that an error has been reached unexpectedly while parsing.
+	 *
 	 */
 	public Date createDateFromString(String dateString) throws ParseException{
 		return formater.parse(dateString);
 	}
 	
 	
-	/**Create String from date
-	 * @param date date who will convert to String
+	/**
+	 * Converts a Date object to a string.
+	 * @param date Date object to be converted to String
 	 * @return date as String in format yyyy-MM-dd
 	 */
 	public String createStringFromDate(Date date){
 		return formater.format(date);
 	}
 	
-	/**Round a given double value to given digit number places
-	 * @param value double value to round
-	 * @param places Decimal places to round
-	 * @return
+	/**
+	 * Round a given double value to given digit number places
+	 * @param value double value to be rounded
+	 * @param places Decimal places to be rounded
+	 * @return rounded input value
 	 */
 	public static double round(double value, int places) {
 	    if (places < 0) throw new IllegalArgumentException();
@@ -51,7 +56,12 @@ public enum Tool {
 	    return (double) tmp / factor;
 	}
 
-
+	/**
+	 * Returns a list with the ...
+	 * @param from 
+	 * @param to 
+	 * @return 
+	 */
 	public static List<History> getHistory(String from, String to) throws ClassNotFoundException, SQLException, ParseException {
 		return DAOCurrency.getInstance().getHistory(from,to);
 	}
