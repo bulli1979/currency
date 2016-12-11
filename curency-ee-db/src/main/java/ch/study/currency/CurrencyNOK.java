924462d365910cd4a127698013fc69a5410e9377
gitificial <federer.michi@gmail.com>
2016-12-11 22:02:09 +0100

    Update CurrencyNOK.java

--- curency-ee-db/src/main/java/ch/study/currency/CurrencyNOK.java
+++ curency-ee-db/src/main/java/ch/study/currency/CurrencyNOK.java
@@ -2,6 +2,12 @@ package ch.study.currency;
 
 import java.util.Date;
 
+/**
+ * @author Nicole Wallner
+ * @version 0.1
+ *
+ */
+
 public class CurrencyNOK implements Currency {
 	private static final String SHORTNAME = "NOK";
 	private static final String NAME = "Norwegische Kronen";
