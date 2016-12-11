def8b96ccbaa4b35872b6d094873f45829802bb7
gitificial <federer.michi@gmail.com>
2016-12-11 21:50:56 +0100

    Update CurrencyCAD.java

--- curency-ee-db/src/main/java/ch/study/currency/CurrencyCAD.java
+++ curency-ee-db/src/main/java/ch/study/currency/CurrencyCAD.java
@@ -2,6 +2,12 @@ package ch.study.currency;
 
 import java.util.Date;
 
+/**
+ * @author Nicole Wallner
+ * @version 0.1
+ *
+ */
+
 public class CurrencyCAD implements Currency {
 	private static final String SHORTNAME = "CAD";
 	private static final String NAME = "Kanadischer Dollar";
