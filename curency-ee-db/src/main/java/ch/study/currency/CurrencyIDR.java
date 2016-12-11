ab30d95d225aaa4099b7ba35dd98b56f6e61e99c
gitificial <federer.michi@gmail.com>
2016-12-11 21:59:40 +0100

    Update CurrencyIDR.java

--- curency-ee-db/src/main/java/ch/study/currency/CurrencyIDR.java
+++ curency-ee-db/src/main/java/ch/study/currency/CurrencyIDR.java
@@ -2,6 +2,12 @@ package ch.study.currency;
 
 import java.util.Date;
 
+/**
+ * @author Nicole Wallner
+ * @version 0.1
+ *
+ */
+
 public class CurrencyIDR implements Currency {
 	private static final String SHORTNAME = "IDF";
 	private static final String NAME = "Indonesische Rupiah";
