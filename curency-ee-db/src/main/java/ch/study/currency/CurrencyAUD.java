370042020ffd00a9eaf61cdeba75f0b4c097bf18
gitificial <federer.michi@gmail.com>
2016-12-11 21:49:43 +0100

    Update CurrencyAUD.java

--- curency-ee-db/src/main/java/ch/study/currency/CurrencyAUD.java
+++ curency-ee-db/src/main/java/ch/study/currency/CurrencyAUD.java
@@ -2,6 +2,12 @@ package ch.study.currency;
 
 import java.util.Date;
 
+/**
+ * @author Nicole Wallner
+ * @version 0.1
+ *
+ */
+
 public class CurrencyAUD implements Currency {
 	private static final String SHORTNAME = "AUD";
 	private static final String NAME = "Australischer Dollar";
