0ae0f49fbe58f8bcab8a3bdc0a91ad602611ada0
gitificial <federer.michi@gmail.com>
2016-12-11 22:03:34 +0100

    Update CurrencyRON.java

--- curency-ee-db/src/main/java/ch/study/currency/CurrencyRON.java
+++ curency-ee-db/src/main/java/ch/study/currency/CurrencyRON.java
@@ -2,6 +2,12 @@ package ch.study.currency;
 
 import java.util.Date;
 
+/**
+ * @author Nicole Wallner
+ * @version 0.1
+ *
+ */
+
 public class CurrencyRON implements Currency {
 	private static final String SHORTNAME = "RON";
 	private static final String NAME = "Rumänischer Leu";
