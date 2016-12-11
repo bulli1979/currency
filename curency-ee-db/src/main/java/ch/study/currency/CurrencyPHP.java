050501a7b770a723856ab59a68a56f8ecbdc7551
gitificial <federer.michi@gmail.com>
2016-12-11 22:02:50 +0100

    Update CurrencyPHP.java

--- curency-ee-db/src/main/java/ch/study/currency/CurrencyPHP.java
+++ curency-ee-db/src/main/java/ch/study/currency/CurrencyPHP.java
@@ -2,6 +2,12 @@ package ch.study.currency;
 
 import java.util.Date;
 
+/**
+ * @author Nicole Wallner
+ * @version 0.1
+ *
+ */
+
 public class CurrencyPHP implements Currency {
 	private static final String SHORTNAME = "PHP";
 	private static final String NAME = "Phillipinischer Peso";
