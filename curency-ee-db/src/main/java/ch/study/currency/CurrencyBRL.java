0be6be126b37187f7832d0455cfb52d3bcd51a95
gitificial <federer.michi@gmail.com>
2016-12-11 21:50:29 +0100

    Update CurrencyBRL.java

--- curency-ee-db/src/main/java/ch/study/currency/CurrencyBRL.java
+++ curency-ee-db/src/main/java/ch/study/currency/CurrencyBRL.java
@@ -2,6 +2,12 @@ package ch.study.currency;
 
 import java.util.Date;
 
+/**
+ * @author Nicole Wallner
+ * @version 0.1
+ *
+ */
+
 public class CurrencyBRL implements Currency {
 	private static final String SHORTNAME = "BRL";
 	private static final String NAME = "Brasilianischer Real";
