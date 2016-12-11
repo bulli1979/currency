39e79f4b2438b1c446a352cbd326d53a7fb7ae3d
gitificial <federer.michi@gmail.com>
2016-12-11 22:01:04 +0100

    Update CurrencyKRW.java

--- curency-ee-db/src/main/java/ch/study/currency/CurrencyKRW.java
+++ curency-ee-db/src/main/java/ch/study/currency/CurrencyKRW.java
@@ -2,6 +2,12 @@ package ch.study.currency;
 
 import java.util.Date;
 
+/**
+ * @author Nicole Wallner
+ * @version 0.1
+ *
+ */
+
 public class CurrencyKRW implements Currency {
 	private static final String SHORTNAME = "KRW";
 	private static final String NAME = "Südkoreanischer Won";
