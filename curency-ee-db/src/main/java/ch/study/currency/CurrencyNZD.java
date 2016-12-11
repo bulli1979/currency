2954e605f347153edf9b8d0d88d3cdaed858ddb6
gitificial <federer.michi@gmail.com>
2016-12-11 22:02:31 +0100

    Update CurrencyNZD.java

--- curency-ee-db/src/main/java/ch/study/currency/CurrencyNZD.java
+++ curency-ee-db/src/main/java/ch/study/currency/CurrencyNZD.java
@@ -2,6 +2,12 @@ package ch.study.currency;
 
 import java.util.Date;
 
+/**
+ * @author Nicole Wallner
+ * @version 0.1
+ *
+ */
+
 public class CurrencyNZD implements Currency {
 	private static final String SHORTNAME = "NZD";
 	private static final String NAME = "Neuseeländischer Dollar";
