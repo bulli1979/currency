736eb10ed762798df6d553bba5a94b3a066d8d15
gitificial <federer.michi@gmail.com>
2016-12-11 21:59:10 +0100

    Update CurrencyHUF.java

--- curency-ee-db/src/main/java/ch/study/currency/CurrencyHUF.java
+++ curency-ee-db/src/main/java/ch/study/currency/CurrencyHUF.java
@@ -2,6 +2,12 @@ package ch.study.currency;
 
 import java.util.Date;
 
+/**
+ * @author Nicole Wallner
+ * @version 0.1
+ *
+ */
+
 public class CurrencyHUF implements Currency {
 	private static final String SHORTNAME = "HUF";
 	private static final String NAME = "Ungarischer Forint";
