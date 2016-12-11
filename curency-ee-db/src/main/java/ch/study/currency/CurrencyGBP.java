2976004b237244c13a77e6a4d97ea4fe052c5286
gitificial <federer.michi@gmail.com>
2016-12-11 21:58:03 +0100

    Update CurrencyGBP.java

--- curency-ee-db/src/main/java/ch/study/currency/CurrencyGBP.java
+++ curency-ee-db/src/main/java/ch/study/currency/CurrencyGBP.java
@@ -2,6 +2,12 @@ package ch.study.currency;
 
 import java.util.Date;
 
+/**
+ * @author Nicole Wallner
+ * @version 0.1
+ *
+ */
+
 public class CurrencyGBP implements Currency {
 	private static final String SHORTNAME = "GBP";
 	private static final String NAME = "Britisches Pfund";
